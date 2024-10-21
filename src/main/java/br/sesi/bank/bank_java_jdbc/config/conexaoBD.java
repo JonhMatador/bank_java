0cb556279908c721796786e2a816b91fc9df6441
JonhMatador <sesisamuel15@gmail.com>
2024-10-21 11:42:44 -0300

    configurado e conectado

---
+++ src/main/java/br/sesi/bank/bank_java_jdbc/config/conexaoBD.java
@@ -0,0 +1,17 @@
+package br.sesi.bank.bank_java_jdbc.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class conexaoBD{
+    public static void main(String[] args){
+        try{
+            Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/banco_sesi?user=postgres&password=root");
+            
+            System.out.println("Conseguiu conectar!");
+        }catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
